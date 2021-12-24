9dcb11e81e96899c4288a9acb5cfa13f1081b582
Alexander Brandao <alex12a7x@gmail.com>
2021-12-24 12:31:00 -0300

    agora foi

--- excercicio_um/integer.rb
+++ excercicio_um/integer.rb
@@ -2,7 +2,7 @@ class Integer
   
   
   def to_roman
-    #criando tabela hash para mapear as representações romanas e suas exceções
+    #Criando tabela hash para mapear as representações romanas e suas exceções
     roman_hash = {
       1000 => "M",
       900 => "CM",
@@ -18,25 +18,26 @@ class Integer
       4 => "IV",
       1 => "I"
     } 
-    #instanciando um numero inteiro
+    #Instanciando um numero inteiro
     num = self
 
-    #metodo reduce acumula as operacoes seguintes numa string vazia
+    #Metodo reduce acumula as operacoes seguintes numa string vazia
     roman_hash.reduce("") do |res, (arab, roman)|
-      #o numero é dividido por todas as hash's até encontrar um divisor comum 
+      #O numero é dividido por todas as hash's até encontrar um divisor comum 
       parte_inteira, num = num.divmod(arab)
-      #as keys dos divisores comuns sao acumulados no resultado como hash
+      #As keys dos divisores comuns sao acumulados no resultado como hash
       res << roman * parte_inteira
     end
   end
 end
 
-#testes
+#Testes
 puts 3.to_roman #ok
 puts 1645.to_roman #ok
 puts 3999.to_roman #ok
 
-#observacoes
-#o algoritimo funciona perfeitamente dos numeros 1 até 3999
-#a partir do 4000 existem variacoes nos simbolos que vão alem dos limites do mapeamento inicial, mas acredito que a ideia do desafio foi alcancada 
+#Observacoes
+#O algoritimo funciona perfeitamente dos numeros 1 até 3999
+#O numero 0 nao existia para os romanos, foi uma revolucao matematica posterior
+#A partir do 4000 existem variacoes nos simbolos que vão alem dos limites do mapeamento inicial, mas acredito que a ideia do desafio foi alcancada 
 puts 5000.to_roman #erro devido as limitaçoes da tabela  
